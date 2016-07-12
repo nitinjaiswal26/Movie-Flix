a61f3aa18c494f435e6313931e53a10c6d9ec10f
nitinjaiswal26 <njaiswal@syr.edu>
2016-07-12 11:32:08 -0400

    Nitin Jaiswal Movie Flix Egen Project
    Nitin Jaiswal Movie Flix Egen Project

---
+++ src/main/java/io/egen/api/entity/Movie.java
@@ -0,0 +1,164 @@
+package io.egen.api.entity;
+
+//import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table
+@NamedQueries(@NamedQuery(name = "User.findById", query = "SELECT u FROM Movie u WHERE u.id=:pid"))
+public class Movie {
+
+	@Id
+	@GenericGenerator(strategy = "uuid2", name = "myuuid")
+	@GeneratedValue(generator = "myuuid")
+	private String id;
+	private String Title;
+	private String Year;
+	private String Rated;
+	private String Released;
+	private String Runtime;
+	private String Genre;
+	private String Director;
+	private String Writer;
+	private String Actors;
+	private String Plot;
+	private String Language;
+	private String Country;
+	private String Awards;
+	private String Poster;
+	private String Metascore;
+	private String imdbRating;
+	private String imdbVotes;
+	private String imdbID;
+	private String Type;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getYear() {
+		return Year;
+	}
+	public void setYear(String year) {
+		Year = year;
+	}
+	public String getRated() {
+		return Rated;
+	}
+	public void setRated(String rated) {
+		Rated = rated;
+	}
+	public String getReleased() {
+		return Released;
+	}
+	public void setReleased(String released) {
+		Released = released;
+	}
+	public String getRuntime() {
+		return Runtime;
+	}
+	public void setRuntime(String runtime) {
+		Runtime = runtime;
+	}
+	public String getGenre() {
+		return Genre;
+	}
+	public void setGenre(String genre) {
+		Genre = genre;
+	}
+	public String getDirector() {
+		return Director;
+	}
+	public void setDirector(String director) {
+		Director = director;
+	}
+	public String getWriter() {
+		return Writer;
+	}
+	public void setWriter(String writer) {
+		Writer = writer;
+	}
+	public String getActors() {
+		return Actors;
+	}
+	public void setActors(String actors) {
+		Actors = actors;
+	}
+	public String getPlot() {
+		return Plot;
+	}
+	public void setPlot(String plot) {
+		Plot = plot;
+	}
+	public String getLanguage() {
+		return Language;
+	}
+	public void setLanguage(String language) {
+		Language = language;
+	}
+	public String getCountry() {
+		return Country;
+	}
+	public void setCountry(String country) {
+		Country = country;
+	}
+	public String getAwards() {
+		return Awards;
+	}
+	public void setAwards(String awards) {
+		Awards = awards;
+	}
+	public String getPoster() {
+		return Poster;
+	}
+	public void setPoster(String poster) {
+		Poster = poster;
+	}
+	public String getMetascore() {
+		return Metascore;
+	}
+	public void setMetascore(String metascore) {
+		Metascore = metascore;
+	}
+	public String getImdbRating() {
+		return imdbRating;
+	}
+	public void setImdbRating(String imdbRating) {
+		this.imdbRating = imdbRating;
+	}
+	public String getImdbVotes() {
+		return imdbVotes;
+	}
+	public void setImdbVotes(String imdbVotes) {
+		this.imdbVotes = imdbVotes;
+	}
+	public String getImdbID() {
+		return imdbID;
+	}
+	public void setImdbID(String imdbID) {
+		this.imdbID = imdbID;
+	}
+	public String getType() {
+		return Type;
+	}
+	public void setType(String type) {
+		Type = type;
+	}
+	
+
+	}
---
+++ src/main/java/io/egen/api/service/MovieService.java
@@ -0,0 +1,18 @@
+package io.egen.api.service;
+
+import java.util.List;
+
+import io.egen.api.entity.Movie;
+
+public interface MovieService {
+
+	public List<Movie> findAll();
+
+	public Movie findOne(String id);
+
+	public Movie create(Movie movie);
+
+	public Movie update(String id, Movie movie);
+
+	public void delete(String id);
+}
